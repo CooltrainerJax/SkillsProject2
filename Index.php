a543d703da683ba4686de48d880007b5470d0ee3
Jackie Cochran (Jackie Delic) <110767523+CooltrainerJax@users.noreply.github.com>
2022-10-06 21:22:39 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,47 @@
+<?php
+include 'header.php';
+include 'navigation.php';
+include 'connection.php';
+include 'footer.php';
+?>
+<main>
+
+    <div class ="container">
+        <h2>Come Experience Canada</h2>
+    </div>
+
+    <h3>Upcoming Adventures</h3>
+        <hr>
+
+    <section class="content">
+        <h4> Halifax</h4>
+
+        <p> Date: August 14 2023
+            <br>
+        Duration: 4 days</p>
+
+    <h5>Summary</h5>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta, diam eget gravida eleifend, sem nisi vehicula mauris, 
+        vel commodo diam est in lorem. Nulla facilisi. Maecenas in erat id nisi laoreet porta. Morbi convallis lacus id erat porttitor 
+        euismod. Nunc non massa mi. In nec sapien finibus, maximus nibh vitae, tincidunt quam. Donec sed odio nulla. Etiam vehicula finibus luctus.</p>
+
+    <h4>Sydney</h4>
+
+        <p>Date: September 20, 2023
+            <br>
+        Duration: 3 days
+        </p>
+
+<h5>Summary</h5>
+
+    <p>Aliquam id nisi nisl. Proin pulvinar fermentum ex, a mollis ex egestas eget. Fusce vel pulvinar urna. 
+        Duis leo ipsum, feugiat vel nisl non, fringilla blandit elit. In sit amet molestie metus, tristique vehicula nisi. 
+        Morbi elementum iaculis diam, quis convallis nisl congue a. Pellentesque egestas massa vel mollis dignissim. Quisque lacinia vulputate tortor id condimentum. 
+        Ut sodales rutrum nisl sed luctus. Donec elementum maximus erat at sollicitudin. Maecenas tincidunt fringilla posuere. Sed odio justo, hendrerit at elementum sed,
+         pharetra sit amet lorem.
+    </p>
+
+    </section>
+
+</main>
---
+++ navigation.php
@@ -0,0 +1,25 @@
+
+
+    <nav class="navbar">
+        <div><img src="images/hamburger.png" alt="hamburger icon" height="30px" width="30px" id="clickme"> </div>
+        <div class="navlist">
+            <ul>
+                <li><a href ="index.php">Home</a></li>
+                <li><a href ="book.php">Book Trip</a></li>
+                <li>Admin Login</li>
+            </ul>
+        </div>
+        <h1 class="title">Halifax Canoe and Kayak</h1>
+        <div><img src="images/paddle-white.png" alt="paddle icon" height="30px" width="30px"></div>
+
+    </nav>
+
+
+
+<script>
+    $(document).ready(function(){
+        $("#clickme").click(function(){
+            $(".navlist") .slideToggle();
+        });
+    });
+</script>
